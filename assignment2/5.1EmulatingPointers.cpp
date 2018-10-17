d806d4625a343400fde417b2e2df1145fb5c513e
timolex <timo.surbeck@gmail.com>
2018-10-17 15:13:46 +0200

    5.1EmulatingPointers.cpp: SmartPointer

--- assignment2/5.1EmulatingPointers.cpp
+++ assignment2/5.1EmulatingPointers.cpp
@@ -1,5 +1,5 @@
 //
-// Created by timo on 16.10.18.
+// Created by timo on 17.10.18.
 //
 
 #include <iostream>
@@ -34,23 +34,95 @@ public:
     T* operator->() {
         return object;
     }
-
 };
 
-class exampleClass {
+template<typename T>
+struct SmartPointer {
+
+    int* pointerCounter;
+    T *object;
+    string address;
+
+public:
+    // regular constructor
+    SmartPointer(T &obj) {
+        object = &obj;
+
+        stringstream ss;
+        ss << &obj;
+        address = ss.str();
+
+        pointerCounter = new int(1);
+    }
+
+    // copy constructor
+    SmartPointer(const SmartPointer<T> &sp) {
+        object = sp.object;
+        pointerCounter = sp.pointerCounter;
+
+        address = sp.address;
+
+        (*pointerCounter)++;
+    }
+
+    // destructor
+    ~SmartPointer() {
+        if ((*pointerCounter)-- < 0) {
+            delete object;
+            delete pointerCounter;
+        }
+    }
+
+    string getAddress() {
+        return address;
+    }
+
+    int counter() {
+        return *pointerCounter;
+    }
+
+    T operator*() {
+        return *object;
+    }
+
+    T* operator->() {
+        return object;
+    }
+
+    SmartPointer<T>& operator= (const SmartPointer<T>& sp) {
+        if (this == &sp) return *this;
+
+        // decrease pointerCounter of lvalue
+        --(*pointerCounter);
+
+        // lvalue's pointerCounter now points to the same value as rvalue's pointerCounter
+        pointerCounter = sp.pointerCounter;  // pointerCounter now points to the same value as rvalue's pointerCounter
+        object = sp.object;
+        address = sp.address;
+
+        // increase pointerCounter (which both lvalue & rvalue now point to)
+        ++(*pointerCounter);
+
+        return *this;
+    }
+};
+
+class Object {
 int i;
 public:
-    exampleClass(int n){
+    Object(int n){
      i = n;
     }
 
     int getNumber() {
         return i;
     }
-
 };
 
-int main(){
+void print(SmartPointer<Object> sp) { cout << "number: " << sp->getNumber() << ", counter: " << sp.counter() << endl; }
+
+void dumbPointerTest() {
+    cout << "====== dumbPointerTest start ======" << endl;
 
     int a = 4;
     DumbPointer<int> dumbPointer(a);
@@ -62,12 +134,49 @@ int main(){
     cout << dumbPointer.getAddress() << endl;
 
     int c = 7;
-    exampleClass testObject(c);
-    DumbPointer<exampleClass> dumbPointer1(testObject);
+    Object testObject(c);
+    DumbPointer<Object> dumbPointer1(testObject);
 
     cout << dumbPointer1->getNumber() << endl;
     cout << dumbPointer1.getAddress() << endl;
 
+    cout << "====== dumbPointerTest end ======" << endl << endl;
+}
 
+void smartPointerTest() {
+    cout << "====== smartPointerTest start ======" << endl;
+
+    Object o1(1);       // let's create our 1st object
+    Object o2(2);       // let's create our 2nd object
+
+    SmartPointer<Object> p(o1);    // ref counter is 1 for 1st object
+    cout << "Pointer counter p: " << p.counter() << endl;    // displays 1
+
+    SmartPointer<Object> q(p);     // another smart pointer that points to o1 (overload copy constructor)
+    cout << "Pointer counter p: " << p.counter() << endl;    // displays 2 (two smart pointers refer to o1)
+    cout << "Pointer counter q: " << q.counter() << endl;    // displays 2 (two smart pointers refer to o1)
+
+    SmartPointer<Object> r(o2);    // ref counter is 1 for 2nd object
+    cout << "Pointer counter r: " << r.counter() << endl;    // displays 1
+    q=r;                            // decrease counter for 1st object and
+                                    // increase counter for 2nd object (overload assignment operator)
+
+    cout << "Pointer counter p: " << p.counter() << endl;    // displays 1
+    cout << "Pointer counter q: " << q.counter() << endl;    // displays 2
+    cout << "Pointer counter r: " << r.counter() << endl;    // displays 2
+
+    print(p); // displays 2, and the 1st object, don't delete the object pointed to by p
+    print(q); // displays 3, and the 2nd object, don't delete the object pointed to by q
+    print(r); // displays 3, and the 2nd object, don't delete the object pointed to by r
+    cout << (*p).getNumber() << (*r).getNumber() << endl; // display 1st and 2nd object (overload operator*)
+    cout << p->getNumber() << q->getNumber() << r->getNumber() << endl; // (overload operator->)
+
+    cout << "====== smartPointerTest end ======" << endl;
+} // now the destructors of p, q, and r are called, make sure that 1st
+// and 2nd object is each deleted once (i.e., when the counter reaches 0)
+
+int main(){
+    dumbPointerTest();
+    smartPointerTest();
     return 0;
 }
