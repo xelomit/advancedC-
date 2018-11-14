68256c922139b7398a03ce03bf8e262090ca382a
timolex <timo.surbeck@gmail.com>
2018-11-14 14:03:03 +0100

    L4.2CountingIterator updated

--- assignment4/L4.2CountingIterator/main.cpp
+++ assignment4/L4.2CountingIterator/main.cpp
@@ -10,7 +10,6 @@ void print(int i) {  cout << i << endl;  }
 int main() {
 
     cout << "vector with elements [10, 20)" << endl;
-    // How does this sense the input value type of counter([...])?
     vector<int> v(counter(10), counter(20));
     for (auto ite : v) print(ite);
 
